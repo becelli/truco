3aa51bb818f9bc03f0118729c41a4b4937e27be4
Gustavo Becelli <gustavobecelli@gmail.com>
2022-09-24 23:12:24 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GatewayGateway } from './gateway/gateway.gateway';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, GatewayGateway],
+})
+export class AppModule {}
